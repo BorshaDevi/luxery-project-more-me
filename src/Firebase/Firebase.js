65864b4e5308fd77946f5f12a446d611e333a0cd
Borshadevi <borshadevi27@gmail.com>
2024-04-15 22:53:11 +0600

    add text

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIpsdFjJGFZbd4J5h4L_jDJAgddsss1Sc",
+  authDomain: "luxury-real-project.firebaseapp.com",
+  projectId: "luxury-real-project",
+  storageBucket: "luxury-real-project.appspot.com",
+  messagingSenderId: "640486084797",
+  appId: "1:640486084797:web:6ef7efa0be086abb8267d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
