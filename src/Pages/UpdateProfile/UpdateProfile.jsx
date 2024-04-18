1b03c572e0f7f03b2f28380c1e809b6634e471fe
Borshadevi <borshadevi27@gmail.com>
2024-04-18 09:36:55 +0600

    add text

--- src/Pages/UpdateProfile/UpdateProfile.jsx
+++ src/Pages/UpdateProfile/UpdateProfile.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 
 const UpdateProfile = () => {
+    const {user}=useContext(AuthContext)
+    
     return (
         <div>
             
